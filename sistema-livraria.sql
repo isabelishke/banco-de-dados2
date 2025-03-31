a173c24f6ab7437b74749aca7851699244551e16
isabelishke <isabeli.hanke@escola.pr.gov.br>
2025-03-31 10:41:34 -0300

    Create sistema-livraria.sql

---
+++ sistema-livraria.sql
@@ -0,0 +1,20 @@
+-- Criar o banco de dados
+CREATE DATABASE Biblioteca;
+
+-- Usar o banco de dados criado
+USE Biblioteca;
+
+-- Criar a tabela Clientes
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY,
+    nomeCliente VARCHAR(255),
+    emailCliente VARCHAR(255)
+);
+
+-- Criar a tabela Compras
+CREATE TABLE Compras (
+    CompraID INT PRIMARY KEY,
+    ClienteID INT,
+    NomeLivro VARCHAR(255),
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
